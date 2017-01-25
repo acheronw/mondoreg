01e8df1062a3b2b530fe4346b5e081fd29d88de7
acheronw <szekeresam@gmail.com>
2017-01-25 17:36:58 +0100

    Landing page changed.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
-    render html: t(:under_construct)
-
-    # render html: "MondoCon registration site. Construction under progress"
-  end
 
 end
