1294f63fdc0542cf2f0a1297466fd13792b35e7e
acheronw <szekeresam@gmail.com>
2017-01-25 11:09:58 +0100

    Basic setup for localization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "MondoCon registration site. Construction under progress"
+    render html: t(:under_construct)
+
+    # render html: "MondoCon registration site. Construction under progress"
   end
 
 end
