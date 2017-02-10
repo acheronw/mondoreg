e3f939780a5ea31b61e8b15755e011c625ec757e
acheronw <szekeresam@gmail.com>
2017-02-10 01:12:13 +0100

    set up SendGrid as email provider on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,23 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "mondoreg_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.default_url_options = { host: 'mondoreg.herokuapp.com' }
+
+  # Use the Sendgrid heroku addon for emails:
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.sendgrid.net',
+      port: 587,
+      domain: 'heroku.com',
+      user_name: ENV['SENDGRID_USERNAME'],
+      password: ENV['SENDGRID_PASSWORD'],
+      authentication: 'plain',
+      enable_starttls_auto: true,
+  }
+
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
