481024f6270ae527901d0a116c6bfab658cc6056
acheronw <szekeresam@gmail.com>
2017-03-02 14:19:00 +0100

    TicketOrder quantity is enforced to be positive both in model and order form

--- app/models/ticket_order.rb
+++ app/models/ticket_order.rb
@@ -3,6 +3,9 @@ class TicketOrder < ApplicationRecord
                                      message: "%value is not a valid ticket status" }
   validates :user_id, presence: true
   validates :ticket_id, presence: true
+  validates :quantity, presence: true
+  validates :quantity, :numericality =>  { :greater_than => 0 }
+
 
   belongs_to :user, inverse_of: :ticket_orders
   belongs_to :ticket, inverse_of: :ticket_orders
