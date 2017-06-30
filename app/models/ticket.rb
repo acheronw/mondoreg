6f3c877d4f9a5685190b8e044f4aaa1f36447790
acheronw <szekeresam@gmail.com>
2017-06-30 12:39:00 +0200

    fixed sale opens comparison

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,7 +3,7 @@ class Ticket < ApplicationRecord
   validates :convention_id, presence: true
 
   scope :active, -> { joins(:convention).merge(Convention.active) }
-  scope :on_sale, -> { where("sale_start < ?", Date.today).where("sale_end > ?", Date.today) }
+  scope :on_sale, -> { where("sale_start <= ?", Date.today).where("sale_end > ?", Date.today) }
 
   has_many :ticket_orders, inverse_of: :ticket, dependent: :destroy
 
