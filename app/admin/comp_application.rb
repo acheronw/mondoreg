2a45c487ca6025f2aa9e912ae3f788cfebe9e726
acheronw <szekeresam@gmail.com>
2017-07-05 22:20:10 +0200

    Updated permit params for activeadmin comp_application

--- app/admin/comp_application.rb
+++ app/admin/comp_application.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register CompApplication do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 
-  permit_params :competition_id, :user_id, :character_name, :character_source, :status, :perf_requests, :admin_msg, :appearance_no, :group_name, group_members: []
+  permit_params :competition_id, :user_id, :character_name, :character_source, :status, :perf_requests, :admin_msg, :appearance_no, :group_name, :nickname, :group_name, :group_members, :veteran
 
 
   # Ez teszi lehetővé, hogy az indexben a user sort a user táblából joinolt name mezővel tudjuk sort-olni:
