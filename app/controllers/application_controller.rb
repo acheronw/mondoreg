1539c4ae9bbd99ac50c81f5dddee9bb8d4f6e6fe
acheronw <szekeresam@gmail.com>
2018-05-17 10:52:47 +0200

    Users can accept privacy policy on profile update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :privacy_accepted])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :privacy_accepted])
 
   end
 
--- db/migrate/20180517081407_add_privacy_accepted_to_users.rb
+++ db/migrate/20180517081407_add_privacy_accepted_to_users.rb
@@ -2,5 +2,5 @@ class AddPrivacyAcceptedToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :privacy_accepted, :boolean, default: false
   end
-  
+
 end
