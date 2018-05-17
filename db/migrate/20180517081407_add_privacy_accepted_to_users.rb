518cd1a3f58105933e11ecf32feab59369400c61
acheronw <szekeresam@gmail.com>
2018-05-17 10:35:43 +0200

    Fixed default value conflict with checkbox

--- db/migrate/20180517081407_add_privacy_accepted_to_users.rb
+++ db/migrate/20180517081407_add_privacy_accepted_to_users.rb
@@ -1,11 +1,6 @@
 class AddPrivacyAcceptedToUsers < ActiveRecord::Migration[5.0]
-  def up
-    add_column :users, :privacy_accepted, :boolean, default: true
-    User.update_all(privacy_accepted: false)
+  def change
+    add_column :users, :privacy_accepted, :boolean, default: false
   end
-
-  def down
-    remove_column :users, :privacy_accepted, :boolean
-  end
-
+  
 end
