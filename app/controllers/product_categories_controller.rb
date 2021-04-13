d6dcfa86b6db71c5dfa48c90ca8f73a7278f57f3
acheronw <szekeresam@gmail.com>
2021-04-13 10:46:17 +0200

    paginate products

--- app/controllers/product_categories_controller.rb
+++ app/controllers/product_categories_controller.rb
@@ -2,6 +2,7 @@ class ProductCategoriesController < ApplicationController
 
   def show
     @product_category = ProductCategory.find(params[:id])
+    @products = @product_category.products.order(name: :desc).page(params[:products_page]).per(10)
   end
 
 end
