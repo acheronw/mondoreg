18efae9fb2426a4d7331691f3bdd485bc42fb339
acheronw <szekeresam@gmail.com>
2021-04-26 10:16:49 +0200

    Activeadmin orders now shows products and delivery addresses. Login button instead of Add to Basket for users browsing webshop without login in.

--- app/admin/order.rb
+++ app/admin/order.rb
@@ -2,4 +2,27 @@ ActiveAdmin.register Order do
 
   permit_params :user_id, :status, :comment, :total_price, :date_submitted, :date_shipped
 
+
+  show do
+    attributes_table do
+      row :user
+      row :status
+      row :comment
+      row :total_price
+      if order.delivery_address.present?
+        row :delivery_address
+      end
+
+      table_for order.line_items do
+        column "Items" do |line_item|
+          link_to line_item.product.name, [ :admin, line_item ]
+        end
+        column "Quantity" do |line_item|
+          line_item.quantity
+        end
+      end
+
+    end
+  end
+
 end
