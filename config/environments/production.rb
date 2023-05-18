6d487dc5e5ae6220f7e1f55be17b99dbb5fd8ea1
acheronw <szekeresam@gmail.com>
2023-05-18 11:58:38 +0200

    Changed own url to tickets.mondocon.hu

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.default_url_options = { host: 'mondoreg.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'tickets.mondocon.hu' }
 
   # Use the Sendgrid heroku addon for emails:
   config.action_mailer.delivery_method = :smtp
